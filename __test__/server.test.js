df12adac6a3f2a9dad21ace5b2be9166aa6e32d9
ft7e <ibraheemhamodeh@gmail.com>
2022-05-30 18:32:08 +0300

    done the demo

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,9 +1,25 @@
 'use strict';
 const server = require('../src/server');
 const superTest = require('supertest');
-const { describe } = require('yargs');
 const request = superTest(server.app);
 
 describe('API Testing', () => {
-  test();
+  test('Home page testing ', async () => {
+    const response = await request.get('/');
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual('this route is working');
+  });
+  test('Data route testing', async () => {
+    const response = await request.get('/data');
+    expect(response.status).toEqual(200);
+    expect(typeof response.body).toEqual('object');
+  });
+  test('Handle 404', async () => {
+    const response = await request.get('/mustBeError');
+    expect(response.status).toEqual(404);
+  });
+  test('Handle Internal Error', async () => {
+    const response = await request.get('/bad');
+    expect(response.status).toEqual(500);
+  });
 });
